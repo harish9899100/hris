fa5dc0080b0ea048a2199682314e267b49c4023e
harish khileri <harishkhileri98@gmail.com>
2026-04-10 08:15:55 +0530

    add attendance records at user side

---
+++ app/controllers/attendance_records_controller.rb
@@ -0,0 +1,19 @@
+class AttendanceRecordsController < ApplicationController
+  def index
+    @attendance_records = current_user.employee.attendance_records.order(date: :desc)
+  end
+
+  def create
+    record = current_user.employee.attendance_records.find_or_initialize_by(date: Date.today)
+
+    if record.check_in.nil?
+      record.check_in = Time.current
+      record.status = :present
+    else
+      record.check_out = Time.current
+    end
+
+    record.save!
+    redirect_to attendance_records_path, notice: "Attendance updated"
+  end
+end
