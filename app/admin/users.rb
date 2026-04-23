b996b573271bbf6bf9945c9c9f0fce071141fc3c
harish khileri <harishkhileri98@gmail.com>
2026-04-23 13:42:57 +0530

    add password column at user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,21 +1,6 @@
 ActiveAdmin.register User do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :name, :email, :employee_id #, :api_key, :employee_id, :reset_password_token, :reset_password_sent_at, :remember_created_at
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :email, :api_key, :encrypted_password, :employee_id, :reset_password_token, :reset_password_sent_at, :remember_created_at]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
-  #permit_params :name, :slug, :setting
+  permit_params :name, :email, :employee_id, :password, :password_confirmation #, :api_key, :employee_id, :reset_password_token, :reset_password_sent_at, :remember_created_at
 
   index do
     selectable_column
@@ -36,6 +21,8 @@ ActiveAdmin.register User do
     f.inputs do
       f.input :name
       f.input :email
+      f.input :password
+      f.input :password_confirmation
       f.input :employee
     end
     f.actions
